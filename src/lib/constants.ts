7db2da8ec3927b61fa32de5f3e2dfb606dca48bf
Andrew Henrice <37001647+brenoandrew@users.noreply.github.com>
2025-07-19 16:19:17 +0700

    Project Upload

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const LINKS = {
+  github: "https://github.com/brenoandrew",
+  linkedin: "https://www.linkedin.com/in/brenoandrew",
+  discord: "https://discordapp.com/",
+  email: "mailto:andrewhenrice@dawnwebs.com",
+  instagram: "https://www.instagram.com/brenoandrew",
+  twitter: "https://twitter.com/andrewhenrice",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
